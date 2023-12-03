409e97ba86ce97c22d6d2845695bda597f42c6d9
Blessing Peter <cherubpeters@gmail.com>
2023-12-03 10:52:48 +0100

    Add Spotify font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        CircularSpTitle: "CircularSpTitle",
+        CircularSp: "CircularSp",
+      },
+    },
   },
   plugins: [],
 };
